163dc00cbc86db79709b7a06dfceb588d83bf621
Pratima Singh <prati.86@gmail.com>
2021-08-02 12:50:12 +1000

    Update architecture diagram

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -1,17 +1,7 @@
 <x-frontend>
     <div class="md:flex mt-10 pb-3 border-b">
-        <div class="w-1/2 md:w-1/3 text-center m-auto">
+        <div class="w-full md:w-full text-center m-auto">
             <img src="{{asset('img/user.png')}}"
-                 class="rounded-full"
                  alt="Pratima Singh"/>
-        </div>
-        <div class="w-full md:w-2/3 mt-10 md:ml-10">
-            <h3>Hi</h3>
-            <h1><span class="text-gray-500">I am </span>Pratima Singh</h1>
-            <section class="text-gray-600">
-            I’m a Solutions Architect in Amazon Web Services also referred to as AWS. Prior to working at AWS, I have led Cloud Center of Excellence teams within various AWS customers for almost a decade.
-            My experience has given me a unique understanding of how AWS services can be used to achieve business goals and innovate..
-            </section>
-        </div>
     </div>
 </x-frontend>
